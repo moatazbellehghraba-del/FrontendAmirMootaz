38409eacda588bdebce816c4fccf2f2921e11745
moatazbellehghraba-del <moataz.bellehghraba@gmail.com>
2025-09-30 20:37:15 +0100

    Add SplashScreen and change tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
 import { Stack } from "expo-router";
 import "./global.css";
-
+import { useEffect, useState } from "react";
+import LoadingScreen from "./Components/LoadingScreen";
 export default function RootLayout() {
+  const [isLoading, setIsLoading] = useState(true); // always start loading
+
   const isFirstLaunch = true; // pretend it's first time
   const isLoggedIn = false; // pretend user is not logged in
+  useEffect(() => {
+    // Simulate a 4-second splash/loading delay
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
